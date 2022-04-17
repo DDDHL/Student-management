a59a4493608856318270c9f273ad2bdc1998af58
LHD <Lyouxiang526@163.com>
2022-04-17 15:46:47 +0800

    测试

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    allip: 'http://113.78.195.105:9568',
+    allip: 'http://113.78.195.100:9568',
   },
   mutations: {
     setPath(state) {
